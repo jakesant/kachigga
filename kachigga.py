ac75548972f81d9490afb954cd7c59943e2f4802
Jake Sant <jake.sant.18@um.edu.mt>
2020-11-15 16:07:05 +0100

    Gonna try ascii art with pyfiglet lib

--- kachigga.py
+++ kachigga.py
@@ -1,4 +1,5 @@
 import os
+import pyfiglet
 
 def greet():
     os.system("Hello there, $USER")
@@ -8,6 +9,12 @@ def acceptInput(input):
         greet()
     if(input == "Kachigga"):
         print("Kachigga!")
+
+
+def main():
+    fig = Figlet(font='slant')
+    introtext = fig.renderText("Hello there user!")
+    print(introtext)
 
 print("~~~Enter a command~~~")
 usr_input = input()
